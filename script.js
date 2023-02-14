1c1caca25b87043ccba7c4601b85a4da2133b3f5
Diwas KC <diwas@frisgo.com>
2023-02-14 08:48:53 +0545

    day 3

---
+++ script.js
@@ -0,0 +1,8 @@
+const hamburgerIcon = document.querySelector('.hamburger-icon');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+hamburgerIcon.addEventListener("click", function () {
+    hamburgerIcon.classList.toggle("close-icon");
+    mobileMenu.classList.toggle("active");
+
+})
